8c8fa5aead1340140544cdcc1435c6ece36d4ed5
boydwang <boydkill@gmail.com>
2013-07-08 00:43:29 +0800

    delete batch files

--- DB/ExportDB.bat
+++
@@ -1,7 +0,0 @@
-set PWD=19880411
-set MYSQLPATH="D:\Development\MySQL\MySQL Server 5.6\bin"
-set DBName=easyads
-set CP=%~dp0
-cd /d %MYSQLPATH%
-mysqldump -uroot -p%PWD% -R %DBName%>%CP%%DBName%.sql
-pause 
--- DB/ImportDB.bat
+++
@@ -1,7 +0,0 @@
-set PWD=19880411
-set MYSQLPATH="D:\Development\MySQL\MySQL Server 5.6\bin"
-set DBName=easyads
-set CP=%~dp0
-cd /d %MYSQLPATH%
-mysql -uroot -p%PWD% %DBName%<%CP%%DBName%.sql
-pause 
