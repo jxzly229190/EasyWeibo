1782272a3b2d73a367285a48472351af60e6e57d
boydwang <boydkill@gmail.com>
2013-07-07 23:26:07 +0800

    added EnvHelper

---
+++ EasyWeibo.Helper/IEnvConstHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnvHelper
+{
+	public interface IEnvConstHelper
+	{
+		string BaseUrl { get; }
+		string AuthorizeUrl { get; }
+		string AppKey { get; }
+		string AppSecret { get; }
+	}
+}
